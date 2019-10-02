f478e7a3fd1d9c4b46f88afba0f2e1406b27d6fe
Liwei Cai <cai.lw123@gmail.com>
2019-10-02 11:21:01 -0400

    Add AWD-LSTM training and inference

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ NetworkX 2.4rc1 is not on pip yet. Please follow https://networkx.github.io/docu
 ## Unit test
 
 Install pytest and run `pytest` in the root folder.
+
+## Language Model
+
+We directly use the original [AWD-LSTM](https://github.com/salesforce/awd-lstm-lm) code.
+Training only works with PyTorch 0.4.0, but inference works with PyTorch 1.x (with a bunch of warning).
+
+Use `make_lm_dataset.py` to generate character LM dataset. See the `awd-lstm-lm/README.md` for how to train. See `lm_example.py` for how to inference.
---
+++ make_lm_dataset.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import random
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('test_lines', type=int, default=1000)
+parser.add_argument('input_file', default='data/English.txt')
+parser.add_argument('output_dir', default='data/english_chars')
+args = parser.parse_args()
+
+
+def line_gen():
+    with open(args.input_file, encoding='utf8') as f:
+        for line in f:
+            new_line = ''
+            for char in line.strip().lower():
+                if new_line:
+                    new_line += ' '
+                new_line += '_' if char == ' ' else char
+            yield new_line
+
+os.makedirs(args.output_dir, exist_ok=True)
+lines = line_gen()
+with open(os.path.join(args.output_dir, 'test.txt'), 'w', encoding='utf8') as f:
+    for _, line in zip(range(1000), lines):
+        print(line, file=f)
+with open(os.path.join(args.output_dir, 'valid.txt'), 'w', encoding='utf8') as f:
+    for _, line in zip(range(1000), lines):
+        print(line, file=f)
+with open(os.path.join(args.output_dir, 'train.txt'), 'w', encoding='utf8') as f:
+    for line in lines:
+        print(line, file=f)

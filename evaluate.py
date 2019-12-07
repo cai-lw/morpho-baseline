f716bd7b9550aa28860bd0391a55e3ac92bc151e
Liwei Cai <liweicai@andrew.cmu.edu>
2019-12-07 16:29:04 -0500

    Add option for micro/macro average in evaluation

--- evaluate.py
+++ evaluate.py
@@ -10,7 +10,7 @@ def load_reference(path):
     :returns: Dict of dict of set like {tag: {lemma: {word}}}.
     """
     d = defaultdict(lambda: defaultdict(set))
-    with open(path) as f:
+    with open(path, encoding='utf8') as f:
         for line in f:
             lemma, word, tag = line.split('\t')
             d[tag][lemma].add(word)
@@ -23,7 +23,7 @@ def load_prediction(path):
     :returns: Dict of dict like {tag: {lemma: word}}.
     """
     d = defaultdict(dict)
-    with open(path) as f:
+    with open(path, encoding='utf8') as f:
         for line in f:
             lemma, word, tag = line.split('\t')
             d[tag][lemma] = word
@@ -49,14 +49,29 @@ class Evaluator:
                     if not check_value(value):
                         raise ValueError("'{}' has an invalid value: {}".format(arg_name, value))
 
-    def score(self, prediction, metric='f1'):
+    def _get_metric(self, true_pos, ref_size, prd_size, metric):
+        precision = true_pos / prd_size if prd_size > 0 else 0
+        recall = true_pos / ref_size if ref_size > 0 else 0
+        if metric == 'precision':
+            return precision
+        elif metric == 'recall':
+            return recall
+        elif metric == 'f1':
+            return precision * recall * 2 / (precision + recall) if precision + recall > 0 else 0
+
+    def score(self, prediction, metric='f1', average='macro'):
         """
         :param prediction: The prediction of your model. Dict of dict like {tag: {lemma: word}}.
         :param metric: Metric for calculating per-slot-pair score.
+        :param average: Type of average.
         :returns: The overall score.
         """
         self._check_dict_dict(prediction, lambda s: isinstance(s, str), 'prediction')
-
+        if metric not in ['f1', 'precision', 'recall']:
+            raise ValueError("'metric' must be 'precision', 'recall' or 'f1', got '{}'.".format(metric))
+        if average not in ['micro', 'macro']:
+            raise ValueError("'average' must be 'micro' or 'macro', got '{}'.".format(average))
+        
         graph = nx.Graph()
         prd_nodes = frozenset('prd_' + tag for tag in prediction.keys())
         ref_nodes = frozenset('ref_' + tag for tag in self.reference.keys())
@@ -72,14 +87,10 @@ class Evaluator:
                         true_pos += 1
                 precision = true_pos / len(prd_dict) if len(prd_dict) > 0 else 0
                 recall = true_pos / len(ref_dict) if len(ref_dict) > 0 else 0
-                if metric == 'precision':
-                    tag_score = precision
-                elif metric == 'recall':
-                    tag_score = recall
-                elif metric == 'f1':
-                    tag_score = precision * recall * 2 / (precision + recall) if precision + recall > 0 else 0
+                if average == 'micro':
+                    tag_score = true_pos
                 else:
-                    raise ValueError("'metric' must be 'precision', 'recall' or 'f1', got '%s'." % metric)
+                    tag_score = self._get_metric(true_pos, len(prd_dict), len(ref_dict), metric)
                 graph.add_edge('prd_' + prd_tag, 'ref_' + ref_tag, weight=-tag_score)
 
         matches = minimum_weight_full_matching(graph, prd_nodes)
@@ -87,7 +98,13 @@ class Evaluator:
         for prd_node, ref_node in matches.items():
             if prd_node in prd_nodes:
                 match_score -= graph[prd_node][ref_node]['weight']
-        return match_score / max(len(self.reference), len(prediction))
+        if average == 'micro':
+            total_prd = sum(len(prd_dict) for prd_dict in prediction.values())
+            total_ref = sum(len(ref_dict) for ref_dict in reference.values())
+            final_score = self._get_metric(match_score, total_prd, total_ref, metric)
+            return final_score * len(self.reference) / max(len(self.reference), len(prediction))
+        else:
+            return match_score / max(len(self.reference), len(prediction))
 
 
 if __name__ == '__main__':
@@ -95,9 +112,10 @@ if __name__ == '__main__':
     parser.add_argument('reference', help='The ground truth file')
     parser.add_argument('prediction', help='The prediction file')
     parser.add_argument('--metric', default='recall', help='The metric to report')
+    parser.add_argument('--average', default='macro', choices=['micro', 'macro'], help='Type of averaging.')
     args = parser.parse_args()
     
     reference = load_reference(args.reference)
     prediction = load_prediction(args.prediction)
     evaluator = Evaluator(reference)
-    print(evaluator.score(prediction, args.metric))
+    print(evaluator.score(prediction, args.metric, args.average))
